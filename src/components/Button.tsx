34234a72c8dade93e995f22c27dd7bfad466acf5
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-04-15 16:55:04 -0400

    fix: woops

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = Omit<
@@ -18,7 +19,10 @@ export function Button({
     <button
       data-type={type}
       data-active={active ?? false}
-      className="flex items-center gap-3 rounded-md px-3 py-1 transition-colors disabled:cursor-not-allowed data-[type='primary']:border data-[type='primary']:border-zinc-700 data-[active='true']:bg-zinc-700 data-[type='primary']:bg-zinc-800 data-[type='primary']:hover:bg-zinc-700 data-[type='primary']:disabled:border-zinc-700/50 data-[type='primary']:disabled:bg-zinc-800/50"
+      className={clsx(
+        "flex items-center gap-3 rounded-md px-3 py-1 transition-colors disabled:cursor-not-allowed data-[type='primary']:border data-[type='primary']:border-zinc-700 data-[type='primary']:bg-zinc-800 data-[type='primary']:hover:bg-zinc-700 data-[type='primary']:disabled:border-zinc-700/50 data-[type='primary']:disabled:bg-zinc-800/50",
+        { "bg-zinc-700": active }
+      )}
       {...props}
     >
       {children}
